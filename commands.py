d72f049898ff140be38a2e79ad1d3c0d4e63e230
Crutoi Alexandru <crutoi2001@gmail.com>
2021-10-24 20:04:02 +0300

    Check if data stocking dir exists

--- commands.py
+++ commands.py
@@ -3,6 +3,7 @@ import yfinance as yf
 import sys
 import os
 
+#show existing terminal commands
 def commands_definition():
     global exit_command
 
@@ -11,11 +12,18 @@ def commands_definition():
     print("To exit enter: " + str(exit_command))
     print("To clear data enter: clear")
 
+#clear all stored stock data
 def clear_command():
     for root, dirs, files in os.walk("csv_stock_data"):
         for file in files:
             os.remove(os.path.join(root, file))
 
+#check if the data stocking directory exists
+def is_data_dir():
+    if  os.path.isdir("csv_stock_data") == False:
+        os.mkdir("csv_stock_data")
+        
+#gather stock data about specified stock
 def gather_command(value):
     stock = value#input("Enter the desires stock: ")
     data_list = list()
@@ -30,12 +38,15 @@ def gather_command(value):
     data['ticker'] = stock
     data_list.append(data)
 
+    is_data_dir()
+
     df = pd.concat(data_list)
     df.to_csv('csv_stock_data/'+stock+'.csv')
 
     print(data.head())
     input_command()
 
+#input terminal commands(test use only)
 def input_command():
     value = input()
 

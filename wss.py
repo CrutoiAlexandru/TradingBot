cc7c76f128258b55f4eb95a1886ca53ebaf9053a
Crutoi Alexandru <crutoi2001@gmail.com>
2021-11-02 23:54:27 +0200

    Stream data

--- wss.py
+++ wss.py
@@ -1,14 +1,11 @@
 import websocket
-import _thread
-import time
+import json
 
-BASE_URL    = "https://paper-api.alpaca.markets"
-ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
 ACC_KEY     = 'PKQ107FOKBD3MUNC0XB7'
 SECRET_KEY  = 'hgF4BsJJvutyt86rXiX9kLoRlrjDIyqUggOCn8i7'
-HEADER      = {'APCA-API-KEY-ID' : ACC_KEY, 'APCA-API-SECRET-KEY' : SECRET_KEY}
 
 def on_message(ws, message):
+    print("message:")
     print(message)
 
 def on_error(ws, error):
@@ -18,29 +15,31 @@ def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
 
 def on_open(ws):
-    def run(*args):
-        for i in range(3):
-            time.sleep(1)
-            ws.send("Hello %d" % i)
-        time.sleep(1)
-        ws.close()
-        print("thread terminating...")
-    _thread.start_new_thread(run, ())
+    print("opened")
 
-def auth(ws):
-    data = '{"action": "auth", {HEADER}}'
-    ws.send(data)
-    print(ws, "da")
-    _thread.start_new_thread(auth, ())
+    data = {"action": "authenticate",
+        "data": {
+            "key_id": ACC_KEY,
+            "secret_key": SECRET_KEY,
+        }
+    }
+    ws.send(json.dumps(data))
+
+    get_stock(ws)
+
+def get_stock(ws):
+    listen = {"action" : "listen", "data" : { "streams" : ["T.TSLA"]}}
+
+    ws.send(json.dumps(listen))
 
 if __name__ == "__main__":
-    MARKET_URL = "wss://stream.data.alpaca.markets/v2/iex"
+    socket = "wss://data.alpaca.markets/stream"
 
-    websocket.enableTrace(True)
-    ws = websocket.create_connection(MARKET_URL,
+    ws = websocket.WebSocketApp(socket,
                               on_open=on_open,
                               on_message=on_message,
                               on_error=on_error,
-                              on_close=on_close)
+                              on_close=on_close
+                            )
 
-    auth(ws)
+    ws.run_forever()    

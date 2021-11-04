296e030490c29ea3d69bc4bdb22cd2fba1828d2e
Crutoi Alexandru <crutoi2001@gmail.com>
2021-11-04 19:33:44 +0200

    Find last purchase details

--- alpaca/buy_stocks/buy_history.py
+++ alpaca/buy_stocks/buy_history.py
@@ -1,8 +1,31 @@
 import json
+import requests
+import config
 
 #create a json file that will remember the buying price of a stock
-def buy_history_json(symbol, price):
-    data = json.dumps({symbol:price})
+def buy_history(symbol):
+    ALPACA_URL = "https://paper-api.alpaca.markets/v2/account/activities"
+    
+    data = {"symbol":symbol}
+    data = json.dumps(data)
 
-    with open("trading_data/" + symbol + ".json", "w") as jsonFile:
-        json.dump(data, jsonFile)
+    req = requests.get(ALPACA_URL, data = data, headers = {"APCA-API-KEY-ID" : config.ACC_KEY, "APCA-API-SECRET-KEY" : config.SECRET_KEY})
+
+    #load the response as a json and isolate the last purchase
+    #the last purchase in alpaca api is allways index 0
+    req = json.loads(req.content)
+
+    index = 0
+
+    #check for our symbol
+    while req:
+        if req[index]["symbol"] == symbol:
+            req = req[index]
+            break
+        else:
+            index = index + 1
+
+    if req["side"] == "buy":
+        return True
+    else:
+        return False
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import yfin.high_prices_average        as hpa 
 import yfin.low_prices_average         as lpa
 import alpaca.websocket_connection.wss as ws
-import alpaca.sell_stocks.sell_module  as sm
-import alpaca.buy_stocks.buy_module    as bm
 import alpaca.buy_stocks.buy_history   as bh
+import alpaca.buy_stocks.buy_module    as bm
+import alpaca.sell_stocks.sell_module  as sm
 
 if __name__ == '__main__':
     # bm.buy_stock("TSLA")
-    # sm.sell_stock("TSLA")
+    # sm.sell_stock("AAPL")
+    print(bh.buy_history("AAPL"))
+
+    
     # ws.ws_connection()
     # hpa.high_prices_average()
     # lpa.low_prices_average()

10dd74c02b493bb825c89c609b2d102327e08709
Crutoi Alexandru <crutoi2001@gmail.com>
2021-11-04 11:03:49 +0200

    Add buying and selling files

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import stock_data.high_prices_average as hpa 
 import stock_data.low_prices_average  as lpa
+import alpaca.websocket_connection.wss as ws
 
 if __name__ == '__main__':
+    ws.ws_connection()
     hpa.high_prices_average()
     lpa.low_prices_average()

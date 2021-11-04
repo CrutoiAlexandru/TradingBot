64d82166bcb19dccd4a631b854c5d5c98b0251f8
Crutoi Alexandru <crutoi2001@gmail.com>
2021-11-04 17:43:05 +0200

    Implement stock buying module

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import yfin.high_prices_average        as hpa 
 import yfin.low_prices_average         as lpa
 import alpaca.websocket_connection.wss as ws
+import alpaca.buy_stocks.buy_module    as bm
 
 if __name__ == '__main__':
-    ws.ws_connection()
-    hpa.high_prices_average()
-    lpa.low_prices_average()
+    bm.buy_stock()
+    # ws.ws_connection()
+    # hpa.high_prices_average()
+    # lpa.low_prices_average()
